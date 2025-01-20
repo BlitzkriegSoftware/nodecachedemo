7375b3ee49f14213405e52c309c0c32ad87e4e7d
Stuart Williams <spookdejur@hotmail.com>
2025-01-19 16:48:15 -0800

    Key,1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodecachedemo
+
+1. Make sure docker is running
+
+2. run `scripts\valkey_start.ps1`
+
+3. Run jest tests too see demo at work
+
+## Reference
+
+- https://github.com/redis/node-redis/tree/master/packages/redis
---
+++ scripts/valkey_start.ps1
@@ -0,0 +1,27 @@
+<#
+	Start valkey
+#>
+function Test-DockerIsRunning {
+  $isOk = 0;
+  Try {
+    Get-Process 'com.docker.backend' -ErrorAction Stop;
+    $isOk = 1;
+  }
+  Catch {
+    $isOk = 0;
+  }
+  return $isOk;
+}
+
+$isrunning = Test-DockerIsRunning;
+if(-not $isrunning) {
+  Write-Output "Docker must be running";
+  return;
+}
+
+$valkeyPORT=6379
+$valkeyNAME="d-valkey"
+#docker pull codingpaws/valkey:latest
+docker stop "${valkeyNAME}" 2>&1 | out-null
+docker rm "${valkeyNAME}" 2>&1 | out-null
+docker run --name ${valkeyNAME} -d -p ${valkeyPORT}:${valkeyPORT} codingpaws/valkey
